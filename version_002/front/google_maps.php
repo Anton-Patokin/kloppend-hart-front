7addaefbac0ecdb3a1e25875ba168fb0e1e701fa
Anton-Patokin <patokin.anton@gmail.com>
2017-03-14 11:47:38 +0100

    google maps markers js

--- version_002/front/google_maps.php
+++ version_002/front/google_maps.php
@@ -1,11 +1,11 @@
 <div class="container background_white">
     <?php
-    $url = 'http://localhost/edge/projects/kloppend-hart-antwerpen/version_002/front/application/service/heatmap/getMetricsByTimeRange/2017-01-25%2000:00:00/2017-02-28%2023:00:00';
-
-    $domain = $_SERVER['HTTP_HOST'] . "/";
-    $ch = curl_init($url);
-    echo(curl_exec($ch));
-    curl_close($ch);
+//    $url = 'http://localhost/edge/projects/kloppend-hart-antwerpen/version_002/front/application/service/heatmap/getMetricsByTimeRange/2017-01-25%2000:00:00/2017-02-28%2023:00:00';
+//
+//    $domain = $_SERVER['HTTP_HOST'] . "/";
+//    $ch = curl_init($url);
+//    echo(curl_exec($ch));
+//    curl_close($ch);
 
 
 //    require_once('C:\xampp\htdocs\edge\projects\kloppend-hart-antwerpen\version_002\Settings.php');
@@ -19,10 +19,10 @@
 </div>
 
 <ui-gmap-google-map center="map.center" zoom="map.zoom" draggable="true" options="options">
-    <div ng-if="showMarkers">
         <ui-gmap-markers doCluster="true" models="marker.randomMarkers_0" coords="'self'" icon="marker.icon_0">
         </ui-gmap-markers>
-        <ui-gmap-markers doCluster="true" models="marker.randomMarkers_1" coords="'self'" icon="marker.icon_1">
-        </ui-gmap-markers>
-    </div>
+<!--    <div ng-if="showMarkers">-->
+<!--        <ui-gmap-markers doCluster="true" models="marker.randomMarkers_1" coords="'self'" icon="marker.icon_1">-->
+<!--        </ui-gmap-markers>-->
+<!--    </div>-->
 </ui-gmap-google-map>
--- version_002/front/js/angular_section_angular_google_maps_controller.js
+++ version_002/front/js/angular_section_angular_google_maps_controller.js
@@ -1,14 +1,46 @@
 app.controller("PrimeController", function ($scope, $http, $interval) {
+    var showHeatmapBool = true;
+    var showMarkersBool = true;
+
+    var $slide;
+
+//application states
     var APP_STATE_LOAD_MAP = 0;
     var APP_STATE_LOAD_CURRENT_HOUR = 1;
     var APP_STATE_LOAD_DATA = 2;
     var APP_STATE_DISPLAY_DATA = 3;
     var APP_STATE_LOAD_FUTURE_DATA = 4;
-    var date = new Date();
-    var startHour = date.getHours() - 2;
-    var endHour = date.getHours() + 1;
-    var day = date.getFullYear() + '-' + ('0' + (date.getMonth() + 1)).slice(-2) + '-' + ('0' + date.getDate()).slice(-2);
 
+    var loaded;
+
+//general variables
+    var map;
+    var heatmap;
+    var heatmapData = [];
+    var markersArray = [];
+
+//default date values
+    var start_time = 0;
+    var end_time = 0;
+
+//heatmap Settings
+    var center;
+    var markerOffset  = 17;
+    var heatMapRadius = 35;
+
+    var markerCluster;
+    var loadAjaxConnections = 0;
+
+//default application state
+    var currentAppState = 0;
+    var currentAppStateFunction = null;
+
+    var date = new Date();
+    var startHour = date.getHours() -2;
+    var endHour   = date.getHours() + 1;
+    var day  = date.getFullYear() + '-'  + ('0' + (date.getMonth()+1)).slice(-2) + '-'+ ('0' +date.getDate()).slice(-2);
+
+    var countPastAjaxCalls = 0;
 
     initialize();
 
@@ -57,17 +89,66 @@ app.controller("PrimeController", function ($scope, $http, $interval) {
 
     function getHeatMapData(startDate, endDate, timeRange, cache) {
         var method = 'GET';
-        var url = 'http://localhost/edge/projects/kloppend-hart-antwerpen/version_002/front/application/service/heatmap/getMetricsByTimeRange/2017-01-25%2000:00:00/2017-02-28%2023:00:00'
-
+        console.log(startDate,endDate);
+        var url = 'http://localhost/edge/projects/kloppend-hart-antwerpen/version_002/front/' +
+            'application/service/heatmap/getMetricsByTimeRange/2017-02-01 13:00:00/ 2017-03-13 15:00:00'
         $http(
             {
                 method: method,
                 url: url,
             }
-        ).then(function (response) {
-            console.log(response);
+        ).then(function (result) {
+            var data =result['data'];
+            console.log("marker",data);
+            for(var api in data){
+
+
+                if(!heatmapData.hasOwnProperty(api)) heatmapData[api] = [];
+                if(!markersArray.hasOwnProperty(api)) markersArray[api] = [];
+
+                for(var metric in data[api]){
+
+                    if(!heatmapData[api].hasOwnProperty(metric)) heatmapData[api][metric] = [];
+                    if(!markersArray[api].hasOwnProperty(metric)) markersArray[api][metric] = [];
+
+                    for(var i in data[api][metric]){
+
+                        if(!heatmapData[api][metric].hasOwnProperty(startDate)) heatmapData[api][metric][startDate] = [];
+                        if(!markersArray[api][metric].hasOwnProperty(startDate)) markersArray[api][metric][startDate] = [];
+
+                        var weight;
+                        if(timeRange == 'current') weight = data[api][metric][i].real_time_weight;
+                        if(timeRange == 'past') weight = data[api][metric][i].overall_weight;
+                        if(timeRange == 'future') weight = data[api][metric][i].future_weight;
+                        //weight needs to be an absolute value -> otherwise heatmap will show squares and other polygons
+                        createMarker({
+                                id:data[api][metric][i].poi_id,
+                                latitude: data[api][metric][i].latitude,
+                                longitude: data[api][metric][i].longitude
+                            },
+                            data[api][metric][i].name, data[api][metric][i].name,
+                            data[api][metric][i].marker_type, startDate,
+                            api,
+                            metric,
+                            data[api][metric][i].nid);
+
+                    }
+                }
+            }
+
+
+
+            // console.log(data);
         });
-        console.log(startDate, endDate, timeRange, cache);
+        // console.log(startDate, endDate, timeRange, cache);
+    }
+
+
+$scope.marker_0=[];
+    function createMarker(point, title, content, marker_type, start_time, api, metric, nid) {
+        $scope.marker_0.push(point);
+        console.log($scope.marker_0);
+
     }
 
     function initializeMap() {
@@ -96,11 +177,11 @@ app.controller("PrimeController", function ($scope, $http, $interval) {
         // var markers_1 = [];
         //
         //
-        // $scope.marker = {
-        //     randomMarkers_0: markers_0,
-        //     randomMarkers_1: markers_1,
-        //     icon_0: {url: "images/markers/marker_0.png"},
-        //     icon_1: {url: "images/markers/marker_111.png"},
-        // }
+        $scope.marker = {
+            randomMarkers_0: {},
+            randomMarkers_1: {},
+            icon_0: {url: "images/markers/marker_0.png"},
+            icon_1: {url: "images/markers/marker_111.png"},
+        }
     }
 });
