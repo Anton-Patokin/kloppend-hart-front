067fbad8af4634e3d40011fb37ba823e0c99c3ad
Sam De Wachter <sam.dewachter@student.kdg.be>
2017-03-16 09:17:57 +0100

    added header scripts

--- version_002/front/header.php
+++ version_002/front/header.php
@@ -13,6 +13,10 @@
         src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCrmTkGoBzp--pRgO5vRXIsbXPrk3VMp_w&libraries=places&language=nl&region=BE"
         type="text/javascript"></script>
     <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular-route.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/angular-morris/1.3.0/angular-morris.min.js"></script>
     <script src="js/angular_main.js"></script>
     <script src="js/angular_section_angular_google_maps_controller.js"></script>
     <script src="js/angular_section_1_controller.js"></script>
