42c5d8acb2bd63ea56604ae19a1e5e53e509b663
Sam De Wachter <sam.dewachter@student.kdg.be>
2017-03-16 09:33:27 +0100

    changed navbar to right hrefs

--- version_002/front/navbar.php
+++ version_002/front/navbar.php
@@ -1,12 +1,12 @@
 <div class="navbar navbar-inverse navbar-fixed-left">
     <a class="navbar-brand" href="#">Brand</a>
     <ul class="nav navbar-nav">
-        <li><a href="#section1/horeca/restaurant" class="main-menu section-001"><span>Horeca</span></a></li>
-        <li><a href="#section1/cultuur/gallerij" class="main-menu section-001"><span>Cultuur</span></a></li>
-        <li><a href="#section1/over_de_stad/Openbare-diensten" class="main-menu section-001"><span>Over de stad</span></a></li>
-        <li><a href="#section1/winkel/kleding" class="main-menu section-001"><span>Shopping</span></a></li>
-        <li><a href="#section1/uitgaan/film" class="main-menu section-001"><span>Uitgaan</span></a></li>
-        <li><a href="#section1/vrije_tijd/sport" class="main-menu section-001"><span>Vrije tijd</span></a></li>
+        <li><a href="#section1/horeca/show/restaurant" class="main-menu section-001"><span>Horeca</span></a></li>
+        <li><a href="#section1/cultuur/show/gallerij" class="main-menu section-001"><span>Cultuur</span></a></li>
+        <li><a href="#section1/over_de_stad/show/Openbare-diensten" class="main-menu section-001"><span>Over de stad</span></a></li>
+        <li><a href="#section1/winkel/show/kleding" class="main-menu section-001"><span>Shopping</span></a></li>
+        <li><a href="#section1/uitgaan/show/film" class="main-menu section-001"><span>Uitgaan</span></a></li>
+        <li><a href="#section1/vrije_tijd/show/sport" class="main-menu section-001"><span>Vrije tijd</span></a></li>
         <li><a href="#section7" class="main-menu section-007"><span>Verkeer</span></a></li>        
         <li><a href="#section8" class="main-menu section-008"><span>Weer</span></a></li>
         <li><a href="#sectionvelo" class="main-menu section-010"><span>Velo</span></a></li>
