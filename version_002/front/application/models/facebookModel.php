d2e287ff4fda89b7104e4be998652c65b25a781b
Sam De Wachter <sam.dewachter@student.kdg.be>
2017-03-16 13:43:23 +0100

    changed facebookModel

--- version_002/front/application/models/facebookModel.php
+++ version_002/front/application/models/facebookModel.php
@@ -1,4 +1,5 @@
 <?php
+require_once('C:\xampp\htdocs\edge\projects\kloppend-hart-antwerpen\version_002\Settings.php');
 require_once ROOT . '/aggregated/service/PoiStatsTimeAggregatedService.class.php';
 class facebookModel {
 
