d6fa4395d6d26d336b64558c5cf741fdce7a8ba9
Sam De Wachter <sam.dewachter@student.kdg.be>
2017-03-16 15:43:59 +0100

    added extra call twitter api

--- version_002/twitter/api/TwitterApi.class.php
+++ version_002/twitter/api/TwitterApi.class.php
@@ -4,77 +4,90 @@ namespace twitter\api;
 require_once "BaseTwitterApi.class.php";
 
 class TwitterApi extends BaseTwitterApi {
+    
+  public function __construct() {
+    
+  }
+  
+  public function search($q, $include_entities='false',  $count='15',$result_type='mixed', $lang=null, $locale=null, $geocode=null, $until=null, $since_id=null, $max_id=null){
+      
+      $request      = 'search/tweets.json';
+      $args         = $this->getArgumentNames('search');
+      $requestArgs  = $this->getDefaultArgumentValues($args);
+     
+      foreach($args as $arg){
+          $name = $arg->name;
+          if($$name != null)
+          $requestArgs[$name] = $$name;
+      }
 
-    public function __construct($accessToken, $accessTokenSecret, $key, $secret) {
-        parent::__construct($accessToken, $accessTokenSecret, $key, $secret);
+      return parent::call($request, $requestArgs);
+  }
+  
+  
+  public function searchUsers($q, $include_entities='false',  $count='20', $page=null){
+      
+      $request      = 'users/search.json';
+      $args         = $this->getArgumentNames('searchUsers');
+      $requestArgs  = $this->getDefaultArgumentValues($args);
+     
+      foreach($args as $arg){
+          $name = $arg->name;
+          if($$name != null)
+          $requestArgs[$name] = $$name;
+      }
+
+      return parent::call($request, $requestArgs);
+  }
+  
+  public function searchTweetsUsers($screen_name, $count='10')
+  {
+    $request = 'statuses/user_timeline.json';
+    $args = $this->getArgumentNames('searchTweetsUsers');
+    $requestArgs = $this->getDefaultArgumentValues($args);
+
+    foreach($args as $arg){
+        $name = $arg->name;
+        if($$name != null)
+        $requestArgs[$name] = $$name;
     }
 
-    public function search($q, $include_entities='false',  $count='15',$result_type='mixed', $lang=null, $locale=null, $geocode=null, $until=null, $since_id=null, $max_id=null){
+    return parent::call($request, $requestArgs);
+  }
 
-        $request      = 'search/tweets.json';
-        $args         = $this->getArgumentNames('search');
-        $requestArgs  = $this->getDefaultArgumentValues($args);
+  public function searchTweets($q, $count='10')
+  {
+    $request = 'search/tweets.json';
+    $args = $this->getArgumentNames('searchTweets');
+    $requestArgs = $this->getDefaultArgumentValues($args);
 
-        foreach($args as $arg){
-            $name = $arg->name;
-            if($$name != null)
-                $requestArgs[$name] = $$name;
-        }
-
-        return parent::call($request, $requestArgs);
+    foreach ($args as $arg) {
+      $name = $arg->name;
+      if($$name != null)
+        $requestArgs[$name] = $$name;
     }
-
-
-    public function searchUsers($q, $include_entities='false',  $count='20', $page=null){
-
-        $request      = 'users/search.json';
-        $args         = $this->getArgumentNames('searchUsers');
-        $requestArgs  = $this->getDefaultArgumentValues($args);
-
-        foreach($args as $arg){
-            $name = $arg->name;
-            if($$name != null)
-                $requestArgs[$name] = $$name;
-        }
-
-        return parent::call($request, $requestArgs);
-    }
-
-    public function searchTweetsUsers($q)
-    {
-        $request = 'statuses/user_timeline.json';
-        $args = $this->getArgumentNames('searchTweetsUsers');
-        $requestArgs = $this->getDefaultArgumentValues($args);
-
-        foreach($args as $arg){
-            $name = $arg->name;
-            if($$name != null)
-                $requestArgs[$name] = $$name;
-        }
-
-        return parent::call($request, $requestArgs);
-    }
-
-
-
-    private function getArgumentNames($function){
-        $f = new \ReflectionMethod($this, $function);
-        $args = array();
-        foreach($f->getParameters() as $param){
-            $args[] = $param;
-        }
-        return $args;
-    }
-
-    private function getDefaultArgumentValues($argParams){
-        $args = array();
-        foreach($argParams as $arg){
-            if($arg->isOptional())
-                $args[$arg->name] = $arg->getDefaultValue();
-        }
-        return $args;
-    }
-
+    return parent::call($request, $requestArgs);
+  }
+  
+  
+  private function getArgumentNames($function){
+      $f = new \ReflectionMethod($this, $function);
+      $args = array();
+      foreach($f->getParameters() as $param){
+          $args[] = $param;
+      }
+      return $args;
+  }
+  
+  private function getDefaultArgumentValues($argParams){
+      $args = array();
+      foreach($argParams as $arg){
+          if($arg->isOptional())
+          $args[$arg->name] = $arg->getDefaultValue();
+      }
+      return $args;
+  }
+  
 }
 
 ?>
