c70c3616a898b1249d15426686f705c0003c11bb
Anton-Patokin <patokin.anton@gmail.com>
2017-03-17 14:16:25 +0100

    bug fix can not read property heatmaplayer
    fix head, add sensor on google api

--- version_002/front/header.php
+++ version_002/front/header.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <!--    css-->
     <link rel="stylesheet" href="css/libs/bootstrap.min.css">
-   <link rel="stylesheet" href="css/base.css">
+    <link rel="stylesheet" href="css/base.css">
     <script type="text/javascript" src="js/libs/lodash.min.js"></script>
     <script type="text/javascript" src="js/libs/angular.min.js"></script>
     <script src="js/angular-simple-logger.js"></script>
     <script src="js/angular-google-maps.js"></script>
     <script
-        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCrmTkGoBzp--pRgO5vRXIsbXPrk3VMp_w&libraries=places&language=nl&region=BE"
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCrmTkGoBzp--pRgO5vRXIsbXPrk3VMp_w&libraries=places&language=nl&region=BE&sensor=false&libraries=drawing&libraries=visualization"
         type="text/javascript"></script>
     <script src="js/libs/angular-route.js"></script>
     <script src="js/libs/jquery.min.js"></script>
