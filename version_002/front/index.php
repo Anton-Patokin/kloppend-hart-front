79c8441732e1028d81ddf2df74fae69ce4a74f93
Anton-Patokin <patokin.anton@gmail.com>
2017-03-31 14:33:18 +0200

    delete coments

--- version_002/front/index.php
+++ version_002/front/index.php
@@ -5,35 +5,11 @@ if(isset($_GET['url'])){
     exit();
 }
 ?>
-
 <?php include_once 'header.php'; ?>
 <body ng-app="myApp" ng-controller="PrimeController" ng-class="{'hide-overflow' : showOverflow, 'show-overflow' : !showOverflow}">
-
 		<?php include_once 'navbar.php'; ?>
         <?php include_once 'navbar_top.php'; ?>
 		<div class="ng-view-frame" ng-view></div>    
    		<?php include_once 'google_maps.php' ;?>
-<!--    <ui-gmap-google-map center='map.center' zoom='map.zoom'></ui-gmap-google-map>-->
-        
-
-<!--    fit='true' doCluster="true"-->
-
 <?php include_once 'footer.php'; ?>
 
-
-<!--<body ng-app="myApp">-->
-
-<!--<div >-->
-<!---->
-<!--    <div class="container-fluid">-->
-<!--        <div class="col-md-12 background_white">-->
-<!--            --><?php //include_once 'navbar_top.php'; ?>
-<!--        </div>-->
-<!--    </div>-->
-<!--    <div ng-view></div>-->
-<!--</div>-->
-<!---->
-
-<!---->
-<!---->
-<!---->
