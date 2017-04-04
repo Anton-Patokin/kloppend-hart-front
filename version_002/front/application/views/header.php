58518b628967b30146c9e9b56c9da29532db6b64
Anton-Patokin <patokin.anton@gmail.com>
2017-04-04 12:44:52 +0200

    scale

--- version_002/front/application/views/header.php
+++ version_002/front/application/views/header.php
@@ -1,19 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php
+
+    $rootPath = "https://apen.be/kloppend-hart-antwerpen/front/";
+
+?>
 <head>
     <meta charset="UTF-8">
+    <meta name=viewport content="width=device-width, initial-scale=1">
+
     <!--    css-->
-    <base href="http://localhost/edge/projects/kloppend-hart-antwerpen/version_002/front/">
-    <link rel="stylesheet" href="css/libs/bootstrap.min.css">
-    <link rel="stylesheet" href="css/libs/angular-material.min.css">
-    <link rel="stylesheet" href="css/base.css">
-    <link rel="stylesheet" href="css/libs/rzslider.css">
-    <link rel="stylesheet" href="css/weather-icons.min.css">
-    <link rel="stylesheet" href="css/weather-icons-wind.min.css">
-    <script type="text/javascript" src="js/libs/lodash.min.js"></script>
-    <script type="text/javascript" src="js/libs/angular.min.js"></script>
-    <script src="js/angular-simple-logger.js"></script>
-    <script src="js/angular-google-maps.js"></script>
+    <link rel="stylesheet" href="<?= $rootPath ?>css/libs/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= $rootPath ?>css/libs/angular-material.min.css">
+    <link rel="stylesheet" href="<?= $rootPath ?>css/base.css">
+    <link rel="stylesheet" href="<?= $rootPath ?>css/libs/rzslider.css">
+    <link rel="stylesheet" href="<?= $rootPath ?>css/weather-icons.min.css">
+    <link rel="stylesheet" href="<?= $rootPath ?>css/weather-icons-wind.min.css">
+    <script type="text/javascript" src="<?= $rootPath ?>js/libs/lodash.min.js"></script>
+    <script type="text/javascript" src="<?= $rootPath ?>js/libs/angular.min.js"></script>
+    <script src="<?= $rootPath ?>js/angular-simple-logger.js"></script>
+    <script src="<?= $rootPath ?>js/angular-google-maps.js"></script>
 
     <!--    data picker-->
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular-animate.min.js"></script>
@@ -24,35 +30,27 @@
     <script
         src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCrmTkGoBzp--pRgO5vRXIsbXPrk3VMp_w&libraries=places&language=nl&region=BE&sensor=false&libraries=drawing&libraries=visualization"
         type="text/javascript"></script>
-    <script type="text/javascript" src="js/libs/angular-cookies.min.js"></script>
-    <script src="js/libs/angular-route.js"></script>
-    <script src="js/libs/jquery.min.js"></script>
+    <script type="text/javascript" src="<?= $rootPath ?>js/libs/angular-cookies.min.js"></script>
+    <script src="<?= $rootPath ?>js/libs/angular-route.js"></script>
+    <script src="<?= $rootPath ?>js/libs/jquery.min.js"></script>
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.10.0/ui-bootstrap-tpls.min.js"></script>
-    <script type="text/javascript" src="js/libs/rzslider.js"></script>
-    <script src="js/libs/raphael-min.js"></script>
-    <script src="js/libs/morris.min.js"></script>
-    <script src="js/libs/angular-morris.min.js"></script>
+    <script type="text/javascript" src="<?= $rootPath ?>js/libs/rzslider.js"></script>
+    <script src="<?= $rootPath ?>js/libs/raphael-min.js"></script>
+    <script src="<?= $rootPath ?>js/libs/morris.min.js"></script>
+    <script src="<?= $rootPath ?>js/libs/angular-morris.min.js"></script>
     <!-- <script type="text/javascript" src="js/libs/masonry.pkgd.min.js"></script> -->
-
-    <script src="js/angular_main.js"></script>
-    <script src="js/angular_section_angular_google_maps_controller.js"></script>
-    <script src="js/searchFormController.js"></script>
-    <script src="js/angular_section_1_controller.js"></script>
-    <script src="js/angular_section_7_controller.js"></script>
-    <script src="js/angular_section_8_controller.js"></script>
-    <script src="js/weatherController.js"></script>
-
-    <script type="text/javascript" src="js/angular_nav_top_trending_controller.js"></script>
-    <script type="text/javascript" src="js/angular_side_bar_controller.js"></script>
-    <script type="text/javascript" src="js/global_var.js"></script>
-    <script type="text/javascript" src="js/time_lineController.js"></script>
-</head>
-<?php
-    if (file_exists('application/views/index.php')) {
-        include_once 'application/views/index.php';
-        // var_dump(file("application/views/index.php"));
-    } else {
-        echo "FALSEEE";
-    }
     
-?>
+    <script src="<?= $rootPath ?>js/angular_main.js"></script>
+    <script src="<?= $rootPath ?>js/angular_section_angular_google_maps_controller.js"></script>
+    <script src="<?= $rootPath ?>js/searchFormController.js"></script>    
+    <script src="<?= $rootPath ?>js/angular_section_1_controller.js"></script>
+    <script src="<?= $rootPath ?>js/angular_section_7_controller.js"></script>
+    <script src="<?= $rootPath ?>js/angular_section_8_controller.js"></script>
+    <script src="<?= $rootPath ?>js/weatherController.js"></script>
+
+    <script type="text/javascript" src="<?= $rootPath ?>js/angular_nav_top_trending_controller.js"></script>
+    <script type="text/javascript" src="<?= $rootPath ?>js/angular_side_bar_controller.js"></script>
+    <script type="text/javascript" src="<?= $rootPath ?>js/global_var.js"></script>
+    <script type="text/javascript" src="<?= $rootPath ?>js/time_lineController.js"></script>
+</head>
+<?php include_once 'application/views/index.php'; ?>
