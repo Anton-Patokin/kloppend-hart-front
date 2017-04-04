6d8e79137efe90da9272a18d0aebd2ce076538a0
Anton-Patokin <patokin.anton@gmail.com>
2017-04-04 13:59:10 +0200

    Revert "Remove search after click on mobile version"
    This reverts commit 7e5c83c3bcd3eb1f2d003de78ded3cb703531177.

--- version_002/front/js/searchFormController.js
+++ version_002/front/js/searchFormController.js
@@ -1,35 +1,33 @@
 app.controller("searchFormController", function ($scope, $timeout, $http) {
 
-    $scope.searchNode = function () {
-        $scope.searchResults = [];
-        $scope.showResults;
-        $http({
-            method: 'GET',
-            dataType: 'json',
-            async: true,
-            cache: false,
-            url: 'search/getSearchResults/' + $scope.searchInput
-        }).then(function successCallback(response) {
-            data = response.data;
-            if ($scope.searchInput != '') {
-                $scope.showResults = true;
-                for (var i = 0; i < 10; i++) {
-                    if (data[i] != null) {
-                        $scope.searchResults.push(data[i]);
-                    }
-                }
-            } else {
-                $scope.showResults = false;
-                $scope.searchResults = [];
-            }
-        }, function errorCallback(response) {
-            console.log("Status: " + response);
-        });
-    }
+	$scope.searchNode=function(){
+		$scope.searchResults = [];
+		$scope.showResults;
+		$scope.dropdown = function() {
+			$timeout(function(){
+				$scope.showResults = false;
+			}, 300);
+		};
 
-    $scope.dropdown = function () {
-        $timeout(function () {
-            $scope.showResults = false;
-        }, 300);
-    };
+		$http({
+			method: 'GET',
+			dataType: 'json',
+			async: true,
+			cache: false,
+			url: 'search/getSearchResults/'+$scope.searchInput
+			}).then(function successCallback(response) {
+				data = response.data;
+				if ($scope.searchInput != '') {
+					$scope.showResults = true;
+					for (var i = 0; i < 10; i++) {
+						if (data[i] != null) {$scope.searchResults.push(data[i]);}		    		
+					}
+				} else {
+					$scope.showResults = false;
+					$scope.searchResults = [];
+				}
+			}, function errorCallback(response) {
+				console.log("Status: " + response);
+		});
+	}
 });
