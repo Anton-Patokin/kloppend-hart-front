c9e466dd3e1e837405b9e6d7c5336f4b93e5e8da
Anton-Patokin <patokin.anton@gmail.com>
2017-04-05 11:36:32 +0200

    slider redesign and responsive
    #change color
    #andd blob hour on top of circles

--- version_002/front/application/views/footer.php
+++ version_002/front/application/views/footer.php
@@ -1,18 +1,25 @@
- <div ng-show="showFooter" class="div">
+<div ng-show="showFooter" class="div">
     <div class="overlay_1_1-able" ng-class="{'overlay_1_1-disabled':toggleSlider, 'overlay_1_1-able':!toggleSlider}">
-        <button ng-click="toggleSlider = !toggleSlider" class="custom-slider-button-toggle">tijdlijn — {{showCurrentTime}}</button>
-        <div class="col-md-10">
-            <rzslider class="custom-slider"
-                      rz-slider-model="slider.minValue"
-                      rz-slider-high="slider.maxValue"
-                      rz-slider-options="slider.options">
-            </rzslider>
+        <button ng-click="toggleSlider = !toggleSlider" class="custom-slider-button-toggle">tijdlijn —
+            {{showCurrentTime}}
+        </button>
+        <div class="time_line">
+            <div class="slider_custom">
+                <rzslider class="custom-slider"
+                          rz-slider-model="slider.minValue"
+                          rz-slider-high="slider.maxValue"
+                          rz-slider-options="slider.options">
+                </rzslider>
+            </div>
+            <div class="datapicker_custom">
+                <div class="">
+                    <md-content>
+                        <md-datepicker ng-model="myDate"></md-datepicker>
+                    </md-content>
+                </div>
+            </div>
         </div>
-        <div class="col-md-2 datepicker_1">
-            <md-content>
-                <md-datepicker ng-model="myDate" ></md-datepicker>
-            </md-content>
-        </div>
+
     </div>
 </div>
 </body>
