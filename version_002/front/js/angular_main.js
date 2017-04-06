75f47f1b827dcaf7cb716a1d1a6e4152960ae386
Sam De Wachter <sam.dewachter@student.kdg.be>
2017-04-06 11:44:45 +0200

    removed cookie firstTimeVisited and info

--- version_002/front/js/angular_main.js
+++ version_002/front/js/angular_main.js
@@ -39,35 +39,35 @@ app.config(function ($routeProvider) {
         });
 });
 
-app.run(function run( $rootScope, $cookies ){
-    if ($cookies.get('firstTimeVisited') == null) {
-        $rootScope.firstTimeVisited = true;
-        $rootScope.toggleMenu = function(event){
-            var alreadyHasClass = $("#"+event.target.parentElement.id +" ."+event.target.children[0].classList[0]).hasClass('active-arrow');
+// app.run(function run( $rootScope, $cookies ){
+//     if ($cookies.get('firstTimeVisited') == null) {
+//         $rootScope.firstTimeVisited = true;
+//         $rootScope.toggleMenu = function(event){
+//             var alreadyHasClass = $("#"+event.target.parentElement.id +" ."+event.target.children[0].classList[0]).hasClass('active-arrow');
 
-            $('.not-active-arrow').removeClass('active-arrow');
-            if (alreadyHasClass) {
-                $('.not-active-arrow').removeClass('active-arrow');
-            } else {
-                $("#"+event.target.parentElement.id +" ."+event.target.children[0].classList[0]).addClass('active-arrow');
-            }        
+//             $('.not-active-arrow').removeClass('active-arrow');
+//             if (alreadyHasClass) {
+//                 $('.not-active-arrow').removeClass('active-arrow');
+//             } else {
+//                 $("#"+event.target.parentElement.id +" ."+event.target.children[0].classList[0]).addClass('active-arrow');
+//             }        
 
-            $("#"+event.path[1]['id']).next('div').slideToggle();
+//             $("#"+event.path[1]['id']).next('div').slideToggle();
 
-            $("#"+event.path[1]['id']).parent().siblings().children().next().slideUp();
-        }
+//             $("#"+event.path[1]['id']).parent().siblings().children().next().slideUp();
+//         }
 
-        $rootScope.closeWelcome = function(){
-            $rootScope.firstTimeVisited = false;
-        }
-        var now = new Date();
-        var expirationDate = new Date(now.getFullYear(), now.getMonth()+1, now.getDate());
-        $cookies.put('firstTimeVisited', 'true', {'expires': expirationDate});
-    } else {
-        $rootScope.firstTimeVisited = false;
-    }
+//         $rootScope.closeWelcome = function(){
+//             $rootScope.firstTimeVisited = false;
+//         }
+//         var now = new Date();
+//         var expirationDate = new Date(now.getFullYear(), now.getMonth()+1, now.getDate());
+//         $cookies.put('firstTimeVisited', 'true', {'expires': expirationDate});
+//     } else {
+//         $rootScope.firstTimeVisited = false;
+//     }
 
-});
+// });
 
 angular.module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache']).controller('AppCtrl', function() {
     this.myDate = new Date();
