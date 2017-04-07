ce30a67228fe3bc3b0d45afca88abe9a63bda543
Anton-Patokin <patokin.anton@gmail.com>
2017-04-07 10:22:02 +0200

    #root class active bug fix & styling navbar

--- version_002/settings.php
+++ version_002/settings.php
@@ -2,8 +2,8 @@
 
 DEFINE("HTTP_GET","GET");
 DEFINE("HTTP_POST","POST");
-DEFINE('ROOT', 'C:\xampp\htdocs\edge\projects\kloppend-hart-antwerpen\version_002\\');
-// define('ROOT_FRONT', 'C:\xampp\htdocs\edge\projects\kloppend-hart-antwerpen\version_002\front\\');
+//DEFINE('ROOT', 'C:\xampp\htdocs\edge\projects\kloppend-hart-antwerpen\version_002\\');
+ define('ROOT_FRONT', 'C:\xampp\htdocs\edge\projects\kloppend-hart-antwerpen\version_002\front\\');
 set_time_limit(120);
 global $pdo;
 	try {
